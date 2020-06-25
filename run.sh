3ccc8d2b1cbd0f96e2db99bdfa7e1853422577a5
PajarKharisma <pajarkharisma@hotmail.com>
2020-06-25 16:17:01 +0700

    add log dir

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@
 #SBATCH --mem=64GB
 #SBATCH --gres=gpu:2
 #SBATCH --time=20:00:00
-#SBATCH --output=log/result/result-%j.out
-#SBATCH --error=log/result/result-%j.err
+#SBATCH --output=log/result-%j.out
+#SBATCH --error=log/rresult-%j.err
 
 #%Module
 module load tensorrt/6-cuda10.2
