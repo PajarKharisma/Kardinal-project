92c35d558e5262e27a1aa5efcf9b49b947234379
PajarKharisma <pajarkharisma@hotmail.com>
2020-06-26 17:30:21 +0700

    test siamese

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ else:
         ret, frame = cap.read()
         if ret == True:
             curr_frame = cap.get(cv2.CAP_PROP_POS_FRAMES)
-            img = kardinal.yolov3(frame)
-            # img = kardinal.detected(frame, curr_frame)
+            # img = kardinal.yolov3(frame)
+            img = kardinal.detected(frame, curr_frame)
             out.write(img)
             # cv2.imshow('Frame', img)
             if cv2.waitKey(25) & 0xFF == ord('q'):
